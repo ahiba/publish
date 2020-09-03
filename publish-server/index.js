9dea95ea7e97b742139d73944cc6b9af63a551e9
詹周东 <zhanzhoudong@adpanshi.com>
2020-09-03 23:15:28 +0800

    improve display-system

--- publish-server/index.js
+++ publish-server/index.js
@@ -1,17 +1,31 @@
 const http = require('http');
 const net = require('net');
 const { URL } = require('url');
-const fs = require('fs')
+const fs = require('fs');
+const unzip = require('unzipper');
+
 
 // Create an HTTP tunneling proxy
 const server = http.createServer((req, res) => {
-  console.log(req)
-  let matched = req.url.match(/filename=([^&]+)/);
-  let filename = matched && matched[1]
-  if(!filename) return
-  console.log('filename', filename)
-  let writeStream = fs.createWriteStream("../server/public/" + filename)
-  req.pipe(writeStream)
+  // console.log(req)
+  // let matched = req.url.match(/filename=([^&]+)/);
+  // let filename = matched && matched[1]
+  // if(!filename) return
+  // console.log('filename', filename)
+  // let writeStream = fs.createWriteStream("../server/public/package")
+
+  // let writeStream =  unzip.Extract({path: '../server/public'});
+  let writeStream = unzip.Extract({ path: '../server/public' });
+
+  // req.pipe(writeStream);
+   
+  req.on('data', trunk => {
+    writeStream.write(trunk)
+  })
+  req.on('end', trunk => {
+    writeStream.end(trunk)
+  })
+
   req.on('end', () => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('okay');
--- publish-tool/publish.js
+++ publish-tool/publish.js
@@ -1,43 +1,81 @@
 const http = require('http');
 const queryString = require('querystring');
+const fs = require('fs');
+var archiver = require('archiver');
 
-const postData = queryString.stringify({
-  'content': 'hello world 123'
-})
+let packname = './package';
+
+// fs.stat(filename, (error, stat) =>{
+  const options = {
+    host: 'localhost',
+    port: 3001,
+    path: '/?filename=' + 'package.zip',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      // 'Content-Length': 0
+    }
+  }
+
+  var archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
+  });
+
+  archive.directory(packname, false);
+
+ 
+
+  // archive.on('end', () => {
+  //   console.log("end")
+  // });
+
+  archive.finalize();
+
+  const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    // res.setEncoding('utf8');
+    // res.on('data', (chunk) => {
+    //   console.log(`BODY: ${chunk}`);
+    // });
+    // res.on('end', () => {
+    //   console.log('No more data in response.');
+    // });
+  });
+
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+  
+  archive.pipe(req);
+
+  archive.on('end', () => {
+    req.end()
+  })
+  
+  // Write data to request body
+  // let readStream = fs.createReadStream("./eva.jpeg");
+  // readStream.pipe(req)
+  // readStream.on('end', () => {
+  //   req.end()
+  // })
+  // req.write(postData);
+  // req.end();
+
+  
+  // console.log('stat', stat)
+// })
+
+// const postData = queryString.stringify({
+//   'content': 'hello world 123'
+// })
 
 // const postData = "hello world yes"
 
-const options = {
-  host: 'localhost',
-  port: 3001,
-  path: '/?filename=z.html',
-  method: 'post',
-  headers: {
-    'Content-Type': 'application/octet-stream',
-    'Content-Length': Buffer.byteLength(postData)
-  }
-}
 
 // const req = http.request(options);
 // console.log('执行了 otpions', options)
 // req.end();
 
-const req = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  // res.setEncoding('utf8');
-  // res.on('data', (chunk) => {
-  //   console.log(`BODY: ${chunk}`);
-  // });
-  // res.on('end', () => {
-  //   console.log('No more data in response.');
-  // });
-});
 
-req.on('error', (e) => {
-  console.error(`problem with request: ${e.message}`);
-});
 
-// Write data to request body
-req.write(postData);
-req.end();
